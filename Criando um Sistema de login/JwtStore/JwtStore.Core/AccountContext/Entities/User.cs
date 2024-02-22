a254a2736433eab239681cfbb00a9fbcbd071dc3
Renan Silva <contato.renan.silva@outlook.com>
2024-02-21 22:48:50 -0300

    Iniciado o modulo criando um sistema de login

---
+++ Criando um Sistema de login/JwtStore/JwtStore.Core/AccountContext/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using JwtStore.Core.SharedContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.AccountContext.Entities
+{
+    public class User : Entity
+    {
+
+    }
+}
